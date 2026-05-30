6e56d1abd6e09707583ec9c5d38f9e9995296990
Satendra <princeworks990@gmail.com>
2026-05-31 02:02:25 +0530

    Added New Cards

--- REACT/LearningProps/src/App.jsx
+++ REACT/LearningProps/src/App.jsx
@@ -4,9 +4,9 @@ import Card from './components/Card'
 const App = () => {
   return (
     <div className='parent'>
-      <Card />
-      <Card />
-      <Card />
+      <Card user="Abhishek Rahangdale" description="Abhishek carries a quiet confidence, kind heart, and humble nature. Like Tobey Maguire's Spider-Man, he stays responsible, resilient, thoughtful, and always ready to help others." image="https://i.pinimg.com/1200x/51/47/2c/51472c8178a0d4f17aecd69ab5a8420e.jpg"/>
+      <Card user= "Prince Yadav" description = "Prince is witty, energetic, and emotionally strong. Like Andrew Garfield's Spider-Man, he combines intelligence with charm, faces challenges bravely, values relationships deeply, and inspires others through resilience." image ="https://i.pinimg.com/736x/f2/6f/71/f26f7163141776f54c0e5a371c340a5f.jpg"/>
+      <Card user="Paritosh Upadhyay" description="Paritosh is cheerful, quick-witted, and determined. Like Tom Holland's Spider-Man, he balances ambition with humility, learns from challenges, stays optimistic, and brings positive energy everywhere." image="https://i.pinimg.com/736x/61/19/73/61197340850e933fedf1ae5c7ba3cbdd.jpg"/>
     </div>
   )
 }
--- REACT/LearningProps/src/components/Card.jsx
+++ REACT/LearningProps/src/components/Card.jsx
@@ -3,12 +3,9 @@ import React from 'react'
 const Card = (props) => {
   return (
     <div className='card'>
-        {/* <img src = {props.img} alt = "img" />
-        <h1>{props.title}</h1>
-        <p>{props.desc}</p> */}
-        <img src="https://i.pinimg.com/736x/f2/6f/71/f26f7163141776f54c0e5a371c340a5f.jpg" alt="img" />
-        <h1>Prince Yadav</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora minus eveniet assumenda aut voluptas vel omnis totam impedit libero. Est repellat sapiente voluptate!</p>
+        <img src = {props.image} alt = "img" />
+        <h1>{props.user}</h1>
+        <p>{props.description}</p>
     </div>
   )
 }
