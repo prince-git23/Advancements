f2ae64ab40e5d28b336c418f35387ee9d2ad371d
Satendra <princeworks990@gmail.com>
2026-05-30 17:00:52 +0530

    Component creation

---
+++ REACT/Introduction To design Components/src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className = "card">
+        <h1>Prince yadav</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.</p>
+    </div>
+  )
+}
+
+export default Card
---
+++ REACT/Introduction To design Components/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className = "navbar">
+      <li><ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul></li>
+    </div>
+  )
+}
+
+export default Navbar
