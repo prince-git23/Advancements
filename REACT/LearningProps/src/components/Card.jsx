8c155307fae500ebc13df86bfb2278b126913d4f
Satendra <princeworks990@gmail.com>
2026-05-31 01:36:05 +0530

    "Creation Of Props"

---
+++ REACT/LearningProps/src/components/Card.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Card = (props) => {
+  return (
+    <div className='card'>
+        {/* <img src = {props.img} alt = "img" />
+        <h1>{props.title}</h1>
+        <p>{props.desc}</p> */}
+        <img src="https://i.pinimg.com/736x/f2/6f/71/f26f7163141776f54c0e5a371c340a5f.jpg" alt="img" />
+        <h1>Prince Yadav</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora minus eveniet assumenda aut voluptas vel omnis totam impedit libero. Est repellat sapiente voluptate!</p>
+    </div>
+  )
+}
+
+export default Card
