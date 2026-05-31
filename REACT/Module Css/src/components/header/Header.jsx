458395785a89d7e5fa99aaed58722bfdd3b2aebf
Satendra <princeworks990@gmail.com>
2026-06-01 03:50:18 +0530

    Added Header With Button To Avoid Conflict btn

---
+++ REACT/Module Css/src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './Header.module.css'
+
+const Header = () => {
+  return (
+    <div className={styles.header}>
+      <h1>If You're Watching This You Should Hire me Up!LOL</h1>
+      <button className={styles.btn}>Like This Repo</button>
+    </div>
+  )
+}
+
+export default Header
