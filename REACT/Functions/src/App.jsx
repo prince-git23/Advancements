4597090f2c061bc88c7f9f121526328bcd46b89c
Satendra <princeworks990@gmail.com>
2026-06-09 15:06:42 +0530

    Trying Different FUnctions

--- REACT/Functions/src/App.jsx
+++ REACT/Functions/src/App.jsx
@@ -4,11 +4,24 @@ const App = () => {
   function btnclicked(){
     console.log("Button CLicked")
   }
+  function MouseEnter(){
+    console.log("Mouse Entered")
+  }
+
   return (
     <div >
       <button onClick={btnclicked} className='m-2 p-2 border rounded-2xl bg-blue-500 hover:bg-amber-300 transition duration-1000 hover:scale-110'>
-        Change Mode
+        Click Me
       </button>
+      <button onMouseEnter={MouseEnter} className='m-2 p-2 border rounded-2xl bg-green-500 hover:bg-amber-300 transition duration-1000 hover:scale-110'>
+        Mouse Entered
+      </button>
+      <input onChange={()=>{
+        console.log("User Is Typing")
+      }} className='m-2 p-2 border rounded-2xl transition duration-1000 hover:scale-110' type='text' placeholder='Enter The Name'/>
+      <input onChange={  function getElement(elem){
+    console.log(elem.target.value)
+  }} className='m-2 p-2 border rounded-2xl transition duration-1000 hover:scale-110' type='text' placeholder='Enter The Name'/>
     </div>
   )
 }
