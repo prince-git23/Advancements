8f0766887d3c59c05264be4f788bb6c499f0da9f
Satendra <princeworks990@gmail.com>
2026-06-11 12:46:20 +0530

    Using USestate with Array and objects

--- REACT/AdvanceUseState/src/App.jsx
+++ REACT/AdvanceUseState/src/App.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const App = () => {
-  const ChangeDetails=()=>{
-
+  const [user, setuser] = useState({username:"Prince",age:19})
+  function ChangeDetails(){
+    setuser({
+      username:"Vidhi",
+      age:20
+    })
   }
+ 
   return (
-    <div>
-      
+    <div > 
+      <h1 className='text-4xl m-5 text-black'> Hello I am {user.username}. <br /> I am at the age of {user.age}.</h1>
+      <button onClick={ChangeDetails} className='m-5 p-5 bg-black text-white rounded-xl text-4xl  hover:bg-gray-500 transition duration-500'>Click Me</button>
     </div>
   )
 }
