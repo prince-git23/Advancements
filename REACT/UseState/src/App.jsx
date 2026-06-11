57e41f06f1aa997656220fa0fab3db23913a2843
Satendra <princeworks990@gmail.com>
2026-06-11 11:48:59 +0530

    UseState for Users

--- REACT/UseState/src/App.jsx
+++ REACT/UseState/src/App.jsx
@@ -2,12 +2,15 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [num, setnum] = useState(10)
+  const [user, setuser] = useState("Vidhi")
   function ChangeNum(){
     setnum(20)
+    setuser("prince")
   }
   return (
     <div>
       <h1>Hello I had a {num}</h1>
+      <h2>USername is {user}</h2>
       <button onClick={ChangeNum}>CLick Me</button>
     </div>
   )
