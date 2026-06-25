e99e21dde519e5babd34dd56c7df14f6a4896319
Satendra <princeworks990@gmail.com>
2026-06-26 03:02:03 +0530

    Using USestate with Array and objects

--- REACT/AdvanceUseState/src/App.jsx
+++ REACT/AdvanceUseState/src/App.jsx
@@ -3,16 +3,24 @@ import React, { useState } from 'react';
 const App = () => {
   const [user, setuser] = useState({username:"Prince",age:19})
   function ChangeDetails(){
-    setuser({
-      username:"Vidhi",
-      age:20
-    })
+    const newNum = {...user}; //Copying earliear username with destructing 
+    newNum.username = "Vidhi"
+    newNum.age = 20
+    setuser(newNum)
+  }
+  const[num,setNum] = useState([10,20,30])
+  const push = () =>{
+    const newNum = {...num}
+    newNum.push(99)
+    setNum(newNum)
   }
  
   return (
     <div > 
       <h1 className='text-4xl m-5 text-black'> Hello I am {user.username}. <br /> I am at the age of {user.age}.</h1>
       <button onClick={ChangeDetails} className='m-5 p-5 bg-black text-white rounded-xl text-4xl  hover:bg-gray-500 transition duration-500'>Click Me</button>
+      <h1 className='text-4xl m-5 text-black'> Hello This is an array {num}</h1>
+      <button onClick={push} className='m-5 p-5 bg-black text-white rounded-xl text-4xl  hover:bg-gray-500 transition duration-500'>Click Me</button>
     </div>
   )
 }
