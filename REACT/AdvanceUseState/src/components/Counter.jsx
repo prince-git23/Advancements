15a804f782963577b19e95afc9e6c1aa5058d975
Satendra <princeworks990@gmail.com>
2026-06-26 12:40:21 +0530

    Batch Management

--- REACT/AdvanceUseState/src/components/Counter.jsx
+++ REACT/AdvanceUseState/src/components/Counter.jsx
@@ -9,10 +9,18 @@ const Counter = () => {
         setNum(num+1)
     }
 
+    const ChangeCount3 = () =>{
+        //Batch Update
+        setNum(prev=>(prev+1))
+        setNum(prev=>(prev+1))
+        setNum(prev=>(prev+1))
+    }
+
   return (
     <div>
       <h1 className='text-4xl m-5 text-black'> {num} </h1>
       <button onClick={ChangeDetails} className='m-5 p-5 bg-black text-white rounded-xl text-4xl  hover:bg-gray-500 transition duration-500'>Count 1</button>
+      <button onClick={ChangeCount3} className='m-5 p-5 bg-black text-white rounded-xl text-4xl  hover:bg-gray-500 transition duration-500'>Count 3</button>
       
     </div>
   )
