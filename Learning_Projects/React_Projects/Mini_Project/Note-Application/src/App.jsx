46ff810f2de0761ddabf5316da119b8027b53b56
Satendra <princeworks990@gmail.com>
2026-06-27 17:01:04 +0530

    Working On Two Way Binding

--- Learning_Projects/React_Projects/Mini_Project/Note-Application/src/App.jsx
+++ Learning_Projects/React_Projects/Mini_Project/Note-Application/src/App.jsx
@@ -1,20 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import RightPart from './components/RightPart'
 
 const App = () => {
-
+  const [note, setNote] = useState("")
+  const [details, setDetails] = useState("")
+  
   const submitHandler = (e) => {
-    console.log("Form has Been Submitted")
+    console.log("Note Heading is:",note)
+    console.log("Details are:",details)
     e.preventDefault()
+
+    setNote("")
+    setDetails("")
   }
+
   return (
     <div className='h-screen bg-black text-white lg:flex' >
       <form onSubmit={(e) => {
         submitHandler(e)
       }} className='flex p-10 flex-col items-start gap-5 lg:w-1/2 lg:border-r-2'>
         <h1 className='text-3xl text-white font-bold py-2'>Add Notes</h1>
-        <input type="text" placeholder='Enter Notes Heading' className='px-5 py-2  w-full border-2 rounded' />
-        <textarea type="text" placeholder='Write Details' className='px-5 py-2 h-32  w-full border-2 rounded' />
+
+        {/* For FIrst Input For heading */}
+        <input type="text" 
+        placeholder='Enter Notes Heading' 
+        className='px-5 py-2  w-full border-2 rounded' 
+        value={note}
+        onChange={(e)=>{
+          setNote(e.target.value)
+        }}/>
+
+        <textarea type="text" 
+        placeholder='Write Details' 
+        className='px-5 py-2 h-32  w-full border-2 rounded' 
+        value={details}
+        onChange={(e)=>{
+          setDetails(e.target.value)
+        }}/>
+
         <button className='bg-white text-black px-5 py-2 border-2 rounded w-full'>Submit</button>
       </form>
       <RightPart/>
