fc0e6a9abe801b444502a7a5d8454c29dd0b50e6
Satendra <princeworks990@gmail.com>
2026-06-27 13:02:49 +0530

    Creating UI

--- Learning_Projects/React_Projects/Mini_Project/Note-Application/src/App.jsx
+++ Learning_Projects/React_Projects/Mini_Project/Note-Application/src/App.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      
+    <div className='h-screen bg-black text-white ' >
+      <form className='flex p-10 flex-col items-start gap-5'>
+        <input type="text" placeholder='Enter Notes Heading' className='px-5 py-2 w-1/2 border-2 rounded'/>
+        <textarea type="text" placeholder='Write Details'className='px-5 py-2 h-32 w-1/2 border-2 rounded' />
+        <button className='bg-white text-black px-5 py-2 w-1/2 border-2 rounded'>Submit</button>
+      </form>
     </div>
   )
 }
