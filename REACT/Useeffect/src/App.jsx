5ef93addf85216686c92ea779ae2cfcf7ada842e
Satendra <princeworks990@gmail.com>
2026-06-29 16:49:28 +0530

    UseEffect On Hover With Num Dependency

--- REACT/Useeffect/src/App.jsx
+++ REACT/Useeffect/src/App.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
 
 const App = () => {
+  const [num, setNum] = useState(0)
+  const [num2, setNum2] = useState(0)
+  
+  //Dependency
+  useEffect(function(){
+    console.log("UseEffect is running...")
+  },[num])
+
+
+
   return (
-    <div>
-      
+    <div className='box'>
+      <h2>{num} {num2}</h2>
+      <button onMouseEnter={()=>{
+         setNum(num+1)
+      }} onMouseLeave={()=>{
+        setNum2(num2+100)
+      }}>Hover Me</button>
+      {/* ALternative Thing */}
+      {/* <button onClick={()=>{
+        setNum(num+1)
+        setNum2(num2+100)
+      }}>Click me</button> */}
+
     </div>
   )
 }
