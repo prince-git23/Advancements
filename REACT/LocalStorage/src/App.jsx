d3e5c53d99e5c3b029d112c9515b6a70691adca5
Satendra <princeworks990@gmail.com>
2026-06-29 15:30:46 +0530

    Local Storage Methods

--- REACT/LocalStorage/src/App.jsx
+++ REACT/LocalStorage/src/App.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
 
 const App = () => {
+  // To clear From local Stoage and Session Storage
+  localStorage.clear()
+  sessionStorage.clear()
+
+  // To Set iem into the Local Stoage
+  localStorage.setItem("user","sarthak")
+  localStorage.setItem("age","18")
+  
+  //To get item from the local storage
+  const user = localStorage.getItem("user")
+  console.log(user)
+  const age = localStorage.getItem("age")
+  console.log(age)
+
+  //To store array of objects
+  const user2 = {
+    user : 'Prince',
+    age : 20,
+    role : 'Software Developer'
+  }
+  localStorage.setItem("user",JSON.stringify(user2))
+
+  const userdetails = JSON.parse(localStorage.getItem('user'))
+  console.log(userdetails)
+
+
+
   return (
     <div>
-      
+      App
     </div>
   )
 }
