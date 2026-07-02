ff14500ef06b2bcecf33783c47939f7a5bbbbefb
Satendra <princeworks990@gmail.com>
2026-07-02 21:01:12 +0530

    Adding Buttom Buttons

--- Learning_Projects/React_Projects/Mini_Project/Gallery-Project/src/App.jsx
+++ Learning_Projects/React_Projects/Mini_Project/Gallery-Project/src/App.jsx
@@ -1,21 +1,26 @@
 import axios from 'axios'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import BottomButtons from './components/BottomButtons'
 
 const App = () => {
   const [userData, setUserData] = useState([])
   const getdata = async () => {
-    const response = await axios.get('https://picsum.photos/v2/list?page=2&limit=15')
+    const response = await axios.get('https://picsum.photos/v2/list?page=2&limit=30')
     setUserData(response.data)
     console.log(response.data)
   }
 
-  let printUserData = "No Available USer"
+  useEffect(function(){
+    getdata()
+  },[])
+
+  let printUserData = <h3 className='text-gray-400'>No User Available</h3>
   if (userData.length > 0) {
     printUserData = userData.map(function (elem, idx) {
       return <div className='group' key={idx}>
         <a href={elem.url}>
           <div className='h-40 w-44  transition duration-1000 group-hover:scale-120 brightness-110'>
-            <img className='h-full w-full object-cover rounded-2xl' src={elem.download_url} alt="Image" />
+            <img className='h-full w-full object-cover rounded-2xl overflow-hidden' src={elem.download_url} alt="Image" />
           </div>
           <h2 className='transition duration-1000 group-hover:opacity-0'>{elem.author}</h2>
         </a>
@@ -23,11 +28,11 @@ const App = () => {
     })
   }
   return (
-    <div className='bg-black h-screen p-4 text-white'>
-      <button onClick={getdata} className='bg-green-600 active:scale-95 mb-3 px-5 py-2 rounded text-white'>Get data</button>
-      <div className='flex flex-wrap gap-10'>
+    <div className='bg-black h-screen p-4 text-white overflow-auto'>
+      <div className='flex flex-wrap gap-10 '>
         {printUserData}
       </div>
+      <BottomButtons/>
     </div>
   )
 }
