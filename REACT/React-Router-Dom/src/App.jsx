aea1afa99b472dae8a927f31ae8325fb5f34c315
Satendra <princeworks990@gmail.com>
2026-07-03 20:55:46 +0530

    Working On Routes

--- REACT/React-Router-Dom/src/App.jsx
+++ REACT/React-Router-Dom/src/App.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
 
 const App = () => {
   return (
     <div>
-      App
+      <div className='nav'>
+        <h2>Prince WorkSpace</h2>
+        <div>
+          <a href="/">Home</a>
+          <a href="/contacts">Contact</a>
+          <a href="/about">About us</a>
+        </div>
+      </div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contacts' element={<Contact />} />
+      </Routes>
     </div>
   )
 }
