c3eb2873288c028a5ae2aa6181d7f78153f73833
Satendra <princeworks990@gmail.com>
2026-07-04 02:50:14 +0530

    Adding 404 Route

--- REACT/Advanced-routing/src/App.jsx
+++ REACT/Advanced-routing/src/App.jsx
@@ -6,6 +6,7 @@ import { Route , Routes } from 'react-router-dom'
 import About from './pages/About'
 import Contact from './pages/Contact'
 import Product from './pages/Product'
+import NotFound from './pages/NotFound'
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
           <Route path='/about' element={<About/>}/>
           <Route path='/contact' element={<Contact/>}/>
           <Route path='/product' element={<Product/>}/>
+          <Route path='*' element={<NotFound/>}/>
         </Routes>
       <Footer/>
     </div>
---
+++ REACT/Advanced-routing/src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1 className='text-red-600 text-3xl p-2 bg-white rounded'>404|Page Not Found</h1>
+    </div>
+  )
+}
+
+export default NotFound
